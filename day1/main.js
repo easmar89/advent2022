560cc533ca81f8a7e0eea5583626d7f03c5f7c91
Elie Asmar <elieasmar89@gmail.com>
2022-12-03 11:04:04 +0100

    day1

---
+++ day1/main.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+const file = fs.readFileSync('input.txt', 'utf-8');
+let array = file.split('\n');
+
+let result = [];
+let sum = 0;
+
+for (let i = 0; i < array.length; i++) {
+  if (array[i] !== '') {
+    sum += +array[i];
+  } else {
+    result.push(sum);
+    sum = 0;
+  }
+}
+
+result.sort((a, b) => b - a);
+
+console.log(result[0] + result[1] + result[2]);
