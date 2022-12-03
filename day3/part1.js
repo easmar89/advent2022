b3a219d84a9bc9fe7b40cfe5fe9907e937b0fb72
Elie Asmar <elieasmar89@gmail.com>
2022-12-03 14:14:11 +0100

    day 3 part1

---
+++ day3/part1.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+let file = fs.readFileSync('input.txt', 'utf-8');
+let inputArray = file.split('\n');
+const PRIORITIES = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let prioritiesSum = 0;
+
+inputArray.forEach((list) => {
+  let half = list.length / 2;
+  let part1 = list.slice(0, half);
+  let part2 = list.slice(half);
+  let duplicates = {};
+
+  for (let i = 0; i < part1.length; i++) {
+    if (part2.includes(part1[i]) && !duplicates[part1[i]]) {
+      prioritiesSum += PRIORITIES.indexOf(part1[i]) + 1;
+      duplicates[part1[i]] = 1;
+    }
+  }
+});
+
+console.log(prioritiesSum);
