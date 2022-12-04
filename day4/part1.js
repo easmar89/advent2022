e1d13099d0a411f2429267475fd199fe821c64cb
Elie Asmar <elieasmar89@gmail.com>
2022-12-04 19:22:14 +0100

    day4

---
+++ day4/part1.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+const file = fs.readFileSync('input.txt', 'utf-8');
+const inputArray = file.replaceAll('-', '\n').replaceAll(',', '\n').split('\n');
+
+let completeOverlaps = 0;
+
+for (let i = 0; i < inputArray.length; i += 4) {
+  if (
+    (+inputArray[i] <= +inputArray[i + 2] &&
+      +inputArray[i + 1] >= +inputArray[i + 3]) ||
+    (+inputArray[i] >= +inputArray[i + 2] &&
+      +inputArray[i + 1] <= +inputArray[i + 3])
+  ) {
+    completeOverlaps++;
+  }
+}
+
+console.log(completeOverlaps);
