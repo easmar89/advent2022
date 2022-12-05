bf5f3a922267d08d82f18d9e86c7dafea5542c7b
Elie Asmar <elieasmar89@gmail.com>
2022-12-05 15:46:56 +0100

    refactoring

--- day3/part1.js
+++ day3/part1.js
@@ -9,12 +9,11 @@ inputArray.forEach((list) => {
   let half = list.length / 2;
   let part1 = list.slice(0, half);
   let part2 = list.slice(half);
-  let duplicates = {};
 
   for (let i = 0; i < part1.length; i++) {
-    if (part2.includes(part1[i]) && !duplicates[part1[i]]) {
+    if (part2.includes(part1[i])) {
       prioritiesSum += PRIORITIES.indexOf(part1[i]) + 1;
-      duplicates[part1[i]] = 1;
+      break;
     }
   }
 });
