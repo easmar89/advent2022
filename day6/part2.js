22c1c23cf66be4eb98f8f06486dc86e2cba66097
Elie Asmar <elieasmar89@gmail.com>
2022-12-07 22:51:04 +0100

    day6

---
+++ day6/part2.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const file = fs.readFileSync('input.txt', 'utf-8');
+
+for (let i = 0; i < file.length; i++) {
+  let result = new Set(file.substring(i, i + 14));
+  if (result.size === 14) {
+    console.log(i + 14);
+    break;
+  }
+}
